0c53a6494376e78a5669caf7b6b32cccaf254af4
DDeis <damien.deis@zenika.com>
2018-08-21 13:44:58 +0200

    Use webpack aliases

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -4,9 +4,9 @@ import 'bootstrap/scss/bootstrap.scss';
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 
+import CommonModule from '@common/common.module';
+import ComponentsModule from '@components/components.module';
 import AppComponent from './app.component';
-import CommonModule from './common/common.module';
-import ComponentsModule from './components/components.module';
 
 import './app.scss';
 
